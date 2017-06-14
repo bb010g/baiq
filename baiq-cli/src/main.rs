57fd5bf6fc6daa1ceb2774fa1491447b673d1802
Brayden Banks <bb010g@gmail.com>
2017-06-14 15:36:03 -0700

    New rustfmt, finishing workspacing

--- baiq-cli/src/main.rs
+++ baiq-cli/src/main.rs
@@ -28,34 +28,45 @@ fn main() {
         .setting(AppSettings::ArgRequiredElseHelp)
         .setting(AppSettings::ColoredHelp)
         .max_term_width(100)
-        .arg(Arg::with_name("end-of-day")
-                 .short("e")
-                 .long("end-of-day")
-                 .help("Sets the sender's end-of-day time (hh:mm)")
-                 .takes_value(true))
-        .arg(Arg::with_name("pretty")
-                 .short("p")
-                 .long("pretty")
-                 .help("Return a pretty-printed representation")
-                 .overrides_with_all(&["json", "json-pretty"]))
-        .arg(Arg::with_name("json")
-                 .short("j")
-                 .long("json")
-                 .help("Return a JSON representation")
-                 .overrides_with_all(&["pretty", "json-pretty"]))
-        .arg(Arg::with_name("json-pretty")
-             .long("json-pretty")
-             .help("Return a pretty-printed JSON representation")
-             .overrides_with_all(&["pretty", "json"]))
-        .arg(Arg::with_name("path")
-                 .help("Sets the BAI file to search")
-                 .requires_all(&["end-of-day"])
-                 .required(true)
-                 .index(1))
+        .arg(
+            Arg::with_name("end-of-day")
+                .short("e")
+                .long("end-of-day")
+                .help("Sets the sender's end-of-day time (hh:mm)")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("pretty")
+                .short("p")
+                .long("pretty")
+                .help("Return a pretty-printed representation")
+                .overrides_with_all(&["json", "json-pretty"]),
+        )
+        .arg(
+            Arg::with_name("json")
+                .short("j")
+                .long("json")
+                .help("Return a JSON representation")
+                .overrides_with_all(&["pretty", "json-pretty"]),
+        )
+        .arg(
+            Arg::with_name("json-pretty")
+                .long("json-pretty")
+                .help("Return a pretty-printed JSON representation")
+                .overrides_with_all(&["pretty", "json"]),
+        )
+        .arg(
+            Arg::with_name("path")
+                .help("Sets the BAI file to search")
+                .requires_all(&["end-of-day"])
+                .required(true)
+                .index(1),
+        )
         .get_matches();
 
-    let end_of_day = args.value_of("end-of-day")
-        .expect("No end-of-day time specified.");
+    let end_of_day = args.value_of("end-of-day").expect(
+        "No end-of-day time specified.",
+    );
     let end_of_day = {
         let (hour, rest) = end_of_day.split_at(2);
         let (rest, minute) = rest.split_at(1);
@@ -78,20 +89,18 @@ fn main() {
         Format::Pretty
     };
 
-    let bytes = file.bytes()
-        .collect::<Result<Vec<_>, _>>()
-        .expect("Byte reading error");
-    let raw_records = baimax::parse::file(bytes.as_slice())
-        .to_result()
-        .expect("Syntax error");
+    let bytes = file.bytes().collect::<Result<Vec<_>, _>>().expect(
+        "Byte reading error",
+    );
+    let raw_records = baimax::parse::file(bytes.as_slice()).to_result().expect(
+        "Syntax error",
+    );
     let file = {
-            let mut parsed_records =
-                raw_records
-                    .iter()
-                    .map(|r| ast::Record::parse(r).expect("Field syntax error"));
-            ast::convert::convert(&mut parsed_records, &end_of_day)
-        }
-        .expect("Error converting record");
+        let mut parsed_records = raw_records.iter().map(|r| {
+            ast::Record::parse(r).expect("Field syntax error")
+        });
+        ast::convert::convert(&mut parsed_records, &end_of_day)
+    }.expect("Error converting record");
 
     match format {
         Format::Pretty => {
